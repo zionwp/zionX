d15a6d06d92af0501290791465c03f21c86c6784
Chris Bunting <sales@zionwp.com>
2014-11-14 11:40:39 -0600

    First Import

---
+++ src/core/nginx.h
@@ -0,0 +1,25 @@
+
+/*
+ * Copyright (C) Igor Sysoev
+ * Copyright (C) Nginx, Inc.
+ */
+
+
+#ifndef _NGINX_H_INCLUDED_
+#define _NGINX_H_INCLUDED_
+
+
+#define nginx_version      1004007
+#define NGINX_VERSION      "1.5.5"
+#define NGINX_VER          "zionX/" NGINX_VERSION
+
+#define ZIONX            "zionX"
+#define zionX_version    1000001
+#define ZIONX_VERSION    "1.1"
+#define ZIONX_VER        ZIONX "/" ZIONX_VERSION
+
+#define NGINX_VAR          "NGINX"
+#define NGX_OLDPID_EXT     ".oldbin"
+
+
+#endif /* _NGINX_H_INCLUDED_ */
